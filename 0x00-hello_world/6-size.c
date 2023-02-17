e137850cd112508eb075aa47f49331bb7f3ce4db
Kete Bismark Kwaku <ketebismarkkwaku@gmail.com>
2023-02-17 05:11:02 -0800

    Corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ long long int t;
 float v;
 
 printf("Size of char: &lu byte(S)\n", (unsigned long)sizeof(p));
-printf("Size of char: &lu byte(S)\n", (unsigned long)sizeof(q));
-printf("Size of char: &lu byte(S)\n", (unsigned long)sizeof(r));
-printf("Size of char: &lu byte(S)\n", (unsigned long)sizeof(t));
-printf("Size of char: &lu byte(S)\n", (unsigned long)sizeof(v));
+printf("Size of int: &lu byte(S)\n", (unsigned long)sizeof(q));
+printf("Size of long int: &lu byte(S)\n", (unsigned long)sizeof(r));
+printf("Size of long long int: &lu byte(S)\n", (unsigned long)sizeof(t));
+printf("Size of float: &lu byte(S)\n", (unsigned long)sizeof(v));
 return (0);
 }
